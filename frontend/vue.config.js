3567c76722a2d747760fc8b01844a0ebb0c5c481
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2023-02-20 19:44:39 -0300

    fix: invalid host header

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    compress: true,
     disableHostCheck: true
   }
 })
