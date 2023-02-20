4994fb6e0ed6c2eab3c9b59d9b4d74b9a58344bf
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2023-02-20 20:19:34 -0300

    feat: adiciona disableHostCheck e ws

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  disableHostCheck: true,
   devServer: {
     proxy: {
       '^/': {
         target: 'https://to-do-list-backend.up.railway.app',
+        ws: true,
         changeOrigin: true
       }
     }
