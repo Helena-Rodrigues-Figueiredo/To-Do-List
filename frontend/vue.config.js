1861e4a0cbdca8353db4f76ba50c9a2264a94672
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2023-02-21 09:59:55 -0300

    feat: remove disableHostCheck do config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  disableHostCheck: true,
   devServer: {
     proxy: {
       '^/': {
