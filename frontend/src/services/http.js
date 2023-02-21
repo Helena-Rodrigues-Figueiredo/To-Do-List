5d3f483484fe43947d5895358b4cde93fc6f371c
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2023-02-21 15:56:44 -0300

    feat: adiciona url caso queria rodar o back local

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
+  // caso queria rodar o backend localmente, utilize a baseURL que está comentada
+  // e comente  a primeira
   baseURL: 'https://to-do-list-backend.up.railway.app/',
+  // baseURL: 'http://localhost:3001',
   headers: {
     'Content-type': 'application/json'
   }
